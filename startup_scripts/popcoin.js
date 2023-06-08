e1c6b0a616db19732941717c5b8511ca3dae5e36
Slameko551 <slameko@proton.me>
2023-06-08 20:19:39 +0300

    Added effects for manyullyn popcoin and fixed the texture.

--- startup_scripts/popcoin.js
+++ startup_scripts/popcoin.js
@@ -4,7 +4,8 @@ console.info('Loaded the popcoin.js for Popcoins')
 onEvent('item.registry', event => {
     event.create('popcorn')
         .displayName('Popcorn')
-        .tooltip("Mmm popcorn.")
+        .tooltip("")
+        .tooltip("§7Mmm popcorn.")
         .group('misc')
         .food(food => {
             food
@@ -15,27 +16,30 @@ onEvent('item.registry', event => {
 
     event.create('copper_popcoin')
         .displayName('Copper Popcoin')
-        .tooltip("Not so valuable popcoin.")
+        .tooltip("§cNot so valuable popcoin.")
         .group('misc')
 
 
     event.create('silver_popcoin')
         .displayName('Silver Popcoin')
-        .tooltip("Your average valuabe popcoin.")
+        .tooltip("")
+        .tooltip("§8Your average valuabe popcoin.")
         .group('misc')
 //test
 
     event.create('golden_popcoin')
         .displayName('Golden Popcoin')
-        .tooltip("Slightly better than average popcoin.")
-        .tooltip("§oThat golden butter sure tastes really good..")
+        .tooltip("")
+        .tooltip("§6Slightly better than average popcoin.")
+        .tooltip("§7§oThat golden butter sure tastes really good..")
         .group('misc')
         .food(food => {
             food
-                .hunger(7)
-                .saturation(5)
-                .effect('farmersdelight:nourishment', 250, 1 ,1)
-                .fastToEat()
+                .hunger(5)
+                .saturation(4)
+                .effect('farmersdelight:nourishment', 2600, 1 ,1)
+                .effect('health_boost', 1200 , 1 ,4)
+                .effect('regeneration' 360, 2, 1)
                 .alwaysEdible()
         })
         .maxStackSize(32)
@@ -43,14 +47,25 @@ onEvent('item.registry', event => {
 
     event.create('manyullyn_popcoin')
         .displayName('Manyullyn Popcoin')
-        .tooltip("Godlike popcoin.")
-        .tooltip("§oYou wonder what happens if you eat it..")
+        .tooltip("")
+        .tooltip("§5Godlike popcoin.")
+        .tooltip("§7§oYou wonder what happens if you eat it..")
         .group('misc')
         .food(food => {
             food
-                .effect('alexsmobs:earthquake', 1, 1, 1)
-                .effect('slowness', 120, 0, 2)
-                .effect('alexsmobs:lava_vision', 75, 1, 1)
+                .hunger(7)
+                .saturation(5)
+                .alwaysEdible()
+                //effect times work as ticks, not seconds!
+                .effect('alexsmobs:earthquake', 60, 1, 1)
+                .effect('slowness', 2400, 0, 2)
+                .effect('alexsmobs:lava_vision', 1500, 1, 1)
+                .effect('farmersdelight:nourishment', 2600, 1 ,1)
+                .effect('health_boost', 2000 , 1 ,7)
+                .effect('regeneration' 600, 3, 1)
+                .effect('resistance', 2000, 2, 1)
+                .effect('strength', 1000, 1, 1)
+                .effect('fire_resistance', 2000, 1)
         })
         .maxStackSize(16)
 })
